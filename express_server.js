cea939fa52bb6b7dac4a80d7fa2c1916141acd88
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-13 02:13:52 +0000

    modify get /new handler, only allow logged in userto create new tiny URLs

--- express_server.js
+++ express_server.js
@@ -51,10 +51,14 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars)
 });
 app.get('/urls/new', (req, res) => {
-  const templateVars = {
-    user: users[req.cookies.user_id]
-  };
-  res.render('urls_new', templateVars);
+  if(!req.cookies.user_id){
+    res.redirect('/login');
+  }else{
+    const templateVars = {
+      user: users[req.cookies.user_id]
+    };
+    res.render('urls_new', templateVars);
+  }
 });
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
